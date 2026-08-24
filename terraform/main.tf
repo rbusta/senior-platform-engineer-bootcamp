515326748760579d0596223f690027a6084d34b0
Rodolfo Bustamante <rodolfo.bustamante@gmail.com>
2026-08-24 14:13:56 -0600

    feat: manage s3 bucket with terraform

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "lab" {
+  bucket = "senior-platform-engineer-bootcamp-2026"
+}
